3ebb0e468f490a721dcfb1377c4d185b5a92dcc4
josemfiguero04 <josemfigueroa04@gmail.com>
2023-08-28 17:03:47 -0400

    add models pokemon and type

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,49 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    imagen: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    vida: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    ataque:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    defensa:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    velocidad:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      
+    },
+    altura:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    peso:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    
+    
+  },
+  {timestamps: false});
 };
+
+
